01ac64a9ee46cece6c69e8109ec473c389de1572
HsuanXyz <HsuanGM@gmail.com>
2017-03-17 14:27:47 +0800

    feat: Added the basic funtion

--- src/components/pincode-input/pincode-component.ts
+++ src/components/pincode-input/pincode-component.ts
@@ -1,8 +1,9 @@
+///<reference path="../../../node_modules/ionic-angular/util/util.d.ts"/>
 import { Component, ElementRef, Renderer, ViewEncapsulation, trigger, state, animate, transition, style } from '@angular/core';
 
 import { Config } from 'ionic-angular/config/config';
 import { GestureController, BlockerDelegate, BLOCK_ALL ,ViewController,Platform,NavOptions,NavParams} from 'ionic-angular';
-import { assert } from 'ionic-angular/util/util';
+import { assert,isNumber } from 'ionic-angular/util/util';
 
 /**
  * @private
@@ -29,12 +30,12 @@ import { assert } from 'ionic-angular/util/util';
     '   <div class="pincode-input">' +
     '     <ion-grid class="pincode-input-grid">' +
     '       <ion-row class="pincode-input-row">' +
-    '         <ion-col style="border-left: 0;"><span class="on"></span></ion-col>' +
-    '         <ion-col></ion-col>' +
-    '         <ion-col></ion-col>' +
-    '         <ion-col></ion-col>' +
-    '         <ion-col></ion-col>' +
-    '         <ion-col></ion-col>' +
+    '         <ion-col style="border-left: 0;"><span [class.on]="isNum(codeArr[0])"></span></ion-col>' +
+    '         <ion-col> <span [class.on]="isNum(codeArr[1])"></span></ion-col>' +
+    '         <ion-col> <span [class.on]="isNum(codeArr[2])"></span></ion-col>' +
+    '         <ion-col> <span [class.on]="isNum(codeArr[3])"></span></ion-col>' +
+    '         <ion-col> <span [class.on]="isNum(codeArr[4])"></span></ion-col>' +
+    '         <ion-col> <span [class.on]="isNum(codeArr[5])"></span></ion-col>' +
     '       </ion-row>' +
     '     </ion-grid>' +
     '   </div>' +
@@ -42,24 +43,24 @@ import { assert } from 'ionic-angular/util/util';
     '   <div class="pincode-button-wrapper">' +
     '     <ion-grid> ' +
     '       <ion-row> ' +
-    '         <ion-col><button ion-button color="light">1</button></ion-col>' +
-    '         <ion-col><button ion-button color="light">2</button></ion-col>' +
-    '         <ion-col><button ion-button color="light">3</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(1)">1</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(2)">2</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(3)">3</button></ion-col>' +
     '       </ion-row>' +
     '       <ion-row> ' +
-    '         <ion-col><button ion-button color="light">4</button></ion-col>' +
-    '         <ion-col><button ion-button color="light">5</button></ion-col>' +
-    '         <ion-col><button ion-button color="light">6</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(4)">4</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(5)">5</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(6)">6</button></ion-col>' +
     '       </ion-row>' +
     '       <ion-row> ' +
-    '         <ion-col><button ion-button color="light">7</button></ion-col>' +
-    '         <ion-col><button ion-button color="light">8</button></ion-col>' +
-    '         <ion-col><button ion-button color="light">9</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(7)">7</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(8)">8</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(9)">9</button></ion-col>' +
     '       </ion-row>' +
     '       <ion-row> ' +
-    '         <ion-col><button ion-button color="light" icon-only><ion-icon name="ios-refresh"></ion-icon></button></ion-col>' +
-    '         <ion-col><button ion-button color="light">0</button></ion-col>' +
-    '         <ion-col><button ion-button color="light" icon-only><ion-icon name="ios-backspace"></ion-icon></button></ion-col>' +
+    '         <ion-col><button ion-button color="light" icon-only (click)="restoreClick()"><ion-icon name="ios-refresh"></ion-icon></button></ion-col>' +
+    '         <ion-col><button ion-button color="light" (click)="numClick(0)">0</button></ion-col>' +
+    '         <ion-col><button ion-button color="light" icon-only (click)="delClick()"><ion-icon name="ios-backspace"></ion-icon></button></ion-col>' +
     '       </ion-row>' +
     '     </ion-grid>' +
     '   </div>' +
@@ -83,6 +84,8 @@ export class PincodeCmp {
     forgotPasswordText?:string;
     hideForgotPassword?:boolean;
   };
+  codeArr:Array<number> = [];
+  maxLen : number = 6;
   enabled: boolean;
   hdrId: string;
   id: number;
@@ -139,7 +142,24 @@ export class PincodeCmp {
     this.enabled = true;
   }
 
+  numClick(num:number){
+    if(num < 0 || num > 9) return;
 
+    if(this.codeArr.length < this.maxLen -1){
+      this.codeArr.push(num);
+    }else if(this.codeArr.length === this.maxLen -1){
+      this.codeArr.push(num);
+      console.log('done');
+    }
+  }
+
+  delClick(){
+    this.codeArr.pop()
+  }
+
+  restoreClick(){
+    this.codeArr = [];
+  }
 
   btnClick(button: any) {
     clearInterval(this.autoInterval);
@@ -169,12 +189,21 @@ export class PincodeCmp {
     }
   }
 
+  isNum(num:any): boolean{
+    if(isNumber(num)){
+      return num <= 9 || num >= 0;
+    }else {
+      return false
+    }
+  }
+
 
   bdClick() {
     if (this.enabled && this.d.enableBackdropDismiss) {
       this.dismiss('backdrop');
     }
   }
+
 
   dismiss(role: any): Promise<any> {
     this.stateExpression = 'off';
