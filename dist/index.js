176e0fe13b1a37e111f92631ed17fe3bfd78cae4
HsuanXyz <HsuanGM@gmail.com>
2017-04-01 17:41:26 +0800

    chore: ionic2-pincode-input 0.0.5
    feat: added hideCancelButton flag (#1)

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
+/**
+ * Created by hsuanlee on 2017/3/19.
+ */
+export * from './pincode-options';
 export { PinCode, PincodeController } from './pincode';
 export { PincodeCmp } from './pincode-component';
 export { PincodeInputModule } from './pincode.module';
--- dist/pincode.js
+++ dist/pincode.js
@@ -1,31 +1,20 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 import { Injectable } from '@angular/core';
+import { App } from 'ionic-angular';
 import { PincodeCmp } from './pincode-component';
 import { isPresent, isFunction } from 'ionic-angular/util/util';
 import { ViewController } from 'ionic-angular';
 /**
  * @private
  */
-var PinCode = (function (_super) {
+export var PinCode = (function (_super) {
     __extends(PinCode, _super);
     function PinCode(app, opts) {
         if (opts === void 0) { opts = {}; }
-        var _this = this;
         opts.enableBackdropDismiss = isPresent(opts.enableBackdropDismiss) ?
             !!opts.enableBackdropDismiss : true;
         opts.hideForgotPassword = isPresent(opts.hideForgotPassword) ?
@@ -36,22 +25,25 @@ var PinCode = (function (_super) {
         opts.title = opts.title || 'Password';
         opts.cancelButtonText = opts.cancelButtonText || 'cancel';
         opts.forgotPasswordText = opts.forgotPasswordText || 'forgot password';
-        _this = _super.call(this, PincodeCmp, opts, null) || this;
-        _this._app = app;
-        _this.isOverlay = true;
-        return _this;
+        _super.call(this, PincodeCmp, opts, null);
+        this._app = app;
+        //noinspection TypeScriptUnresolvedVariable
+        this.isOverlay = true;
     }
     /**
     * @private
     */
     PinCode.prototype.getTransitionName = function (direction) {
         var key = (direction === 'back' ? 'alertLeave' : 'alertEnter');
+        //noinspection TypeScriptUnresolvedVariable
         return this._nav && this._nav.config.get(key);
     };
     PinCode.prototype.setTitle = function (title) {
+        //noinspection TypeScriptUnresolvedVariable
         this.data.title = title;
     };
     PinCode.prototype.setCssClass = function (cssClass) {
+        //noinspection TypeScriptUnresolvedVariable
         this.data.cssClass = cssClass;
     };
     PinCode.prototype.present = function (navOptions) {
@@ -61,8 +53,7 @@ var PinCode = (function (_super) {
     };
     return PinCode;
 }(ViewController));
-export { PinCode };
-var PincodeController = (function () {
+export var PincodeController = (function () {
     function PincodeController(_app) {
         this._app = _app;
     }
@@ -70,10 +61,13 @@ var PincodeController = (function () {
         if (opts === void 0) { opts = {}; }
         return new PinCode(this._app, opts);
     };
+    PincodeController.decorators = [
+        { type: Injectable },
+    ];
+    /** @nocollapse */
+    PincodeController.ctorParameters = [
+        { type: App, },
+    ];
     return PincodeController;
 }());
-PincodeController = __decorate([
-    Injectable()
-], PincodeController);
-export { PincodeController };
 //# sourceMappingURL=pincode.js.map
