392fa4e0d3c58240ed6b782172df62b6cee829be
AndrWeisR <34931916+AndrWeisR@users.noreply.github.com>
2018-02-13 14:44:33 +1030

    Update pincode-options.ts

--- src/pincode-options.ts
+++ src/pincode-options.ts
@@ -1,4 +1,11 @@
-
+// A function to handle a completed PIN.
+// The function takes the completed PIN as a parameter,
+// and returns a Promise which resolves if the PIN is valid,
+// or rejects if the PIN is invalid.
+export interface PincodePinHandler {
+    (pin: string): Promise<any>
+}
+    
 export interface PincodeOpt {
     title?:string,
     cancelButtonText?:string,
@@ -9,5 +16,6 @@ export interface PincodeOpt {
     cssClass?:string,
     encoded?:Function,
     passSize?:number,
-    hideToolbar?: boolean
+    hideToolbar?: boolean,
+    pinHandler?: PincodePinHandler
 }
